d00c7205bd985a53ec90cf52a085c686fb45a445
Emirkan Burak Yılmaz <ebylmz17@gmail.com>
2022-07-17 07:46:57 +0300

    view is complited

---
+++ employee-manager-app/src/app/app.component.ts
@@ -0,0 +1,60 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  
+  public title = "Employee Manager App";
+  
+  public employees!: Employee[];
+  
+  public editEmployee!: Employee;
+  
+  public deleteEmployee!: Employee;
+
+
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      } 
+    )
+  }
+
+  public onOpenModal(employee: Employee, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement("button");
+    button.type = "button";
+    button.style.display = "none";
+    button.setAttribute("data-toggle", "modal");
+
+    if (mode === "add")
+      button.setAttribute("data-target", "#addEmployeeModal");
+    else if (mode === "edit") {
+    console.log("button pressed");
+      this.editEmployee = employee;
+      button.setAttribute("data-target", "#updateEmployeeModal");
+    }
+    else if (mode === "delete") {
+      this.deleteEmployee = employee;
+      button.setAttribute("data-target", "#deleteEmployeeModal");
+    }
+    container?.appendChild(button);
+    button.click();
+  }
+}
